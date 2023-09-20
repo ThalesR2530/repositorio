d735462b8f573abe6dc097ed4e8edc0bf66c6857
ThalesR2530 <thales2rosario@gmail.com>
2023-09-19 21:49:14 -0300

    V2

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-console.log("Hello World!")
-console.log("Olá mundo!!")
+console.log("\nCalculadora\n")
+		let num1
+		let num2
+		let result
+		let sinal 
+
+		console.log("Digite o sinal da operação:")
+		prompt(sinal)
+
+		console.log("Digite o num1:")
+	     prompt(num1)
+	     console.log("Digite o num2:")
+	     prompt(num2)
+
+          switch(sinal){
+          case '+':    
+	     escreva("O resultado é:", num1 + num2)
+	     break
+
+	     caso '-':
+	     escreva("o resultado é:", num1 - num2)
+	     pare
+
+	     caso '*':
+	     escreva("o resultado é:", num1 * num2)
+	     pare
+
+	     caso'/':
+	     escreva("o resultado é:", num1 / num2)
+	     pare
+
+	     caso contrario:
+	     escreva("Operação Inválida!")
+	     pare
+
+
+          }
